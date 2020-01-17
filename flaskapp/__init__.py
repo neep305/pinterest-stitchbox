3c1dc43adf3ac0e2bc24ca8b045ab5b253f97627
neep305 <neep305@gmail.com>
2020-01-18 01:11:25 +0900

    add template

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -13,11 +13,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def response_test():
-    return make_response("Custom Response")
+    return render_template('home.html') # make_response("Custom Response")
 
 
 @app.route("/hello")
 @app.route("/hello/<name>")
 def hello(name=None):
     return render_template('hello.html', name=name)
-
