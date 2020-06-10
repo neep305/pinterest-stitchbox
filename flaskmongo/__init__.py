bae84c9fa38585ca630a0e9fc75adc6ce1f2e282
neep305@gmail.com <neep9669>
2020-06-11 00:05:20 +0900

    add config

--- flaskmongo/__init__.py
+++ flaskmongo/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from config import Config
 
 from flask_mongoengine import MongoEngine
 # from flask_restplus import Api
@@ -18,11 +19,12 @@ app = Flask(__name__)
 app.register_blueprint(example_blueprint)
 app.register_blueprint(auth)
 app.register_blueprint(admin)
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'user',
-    'host': 'localhost',
-    'port': 27017
-}
+# app.config['MONGODB_SETTINGS'] = {
+#     'db': 'user',
+#     'host': 'localhost',
+#     'port': 27017
+# }
+app.config.from_object(Config)
 
 db = MongoEngine()
 db.init_app(app)
