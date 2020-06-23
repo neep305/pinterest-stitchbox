a28c7ee0a95228687011044aab1e837fc61b5858
neep305 <neep305@gmail.com>
2020-06-23 11:48:11 +0900

    add session managemnt

--- flaskmongo/routes.py
+++ flaskmongo/routes.py
@@ -1,11 +1,14 @@
 from flaskmongo import app, db
 from flask import render_template, request, json, Response, redirect, flash, url_for, session, jsonify
 from flaskmongo.models import User, Course, Enrollment
+from flaskmongo.forms import LoginForm, RegisterForm
 
 
 @app.route('/')
+@app.route('/index')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', index=True)
 
 
 # @api.route('/api', '/api/')
@@ -50,9 +53,34 @@ def index():
 #         return jsonify("User is deleted!!")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', login=True)
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        user = User.objects(email=email).first()
+        if user and user.get_password(password):
+            flash(f"{user.first_name}, you are successfully logged in!", "success")
+            session['user_id'] = user.user_id
+            session['password'] = user.first_name
+            return redirect("/index")
+        else:
+            flash("Sorry, something went wrong.", "danger")
+    return render_template('login.html', title="Login", form=form, login=True)
+
+
+@app.route('/logout')
+def logout():
+    session['user_id'] = False
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route('/courses/')
+@app.route('/courses/<term>')
+def courses(term="Spring 2019"):
+    return render_template('courses.html', courseData=courseData, courses=True, term=term)
 
 
 @app.route('/register')
@@ -71,11 +99,6 @@ def enrollment():
 courseData = [{"courseID": "1111", "title": "PHP 111", "description": "Intro to PHP", "credits": "3", "term": "Fall, Spring"}, {"courseID": "2222", "title": "Java 1", "description": "Intro to Java Programming", "credits": "4", "term": "Spring"}, {"courseID": "3333", "title": "Adv PHP 201",
                                                                                                                                                                                                                                                        "description": "Advanced PHP Programming", "credits": "3", "term": "Fall"}, {"courseID": "4444", "title": "Angular 1", "description": "Intro to Angular", "credits": "3", "term": "Fall, Spring"}, {"courseID": "5555", "title": "Java 2", "description": "Advanced Java Programming", "credits": "4", "term": "Fall"}]
 
-
-@app.route('/courses/')
-@app.route('/courses/<term>')
-def courses(term="Spring 2019"):
-    return render_template('courses.html', courseData=courseData, courses=True, term=term)
 
 
 @app.route('/user')
